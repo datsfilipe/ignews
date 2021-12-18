c5cd48269073111e4247dbae1b6b7b867af71815
datsfilipe <datsfilipe@gmail.com>
2021-12-18 18:08:26 -0300

    disable subscribe button

--- src/Components/SubscribeButton/index.tsx
+++ src/Components/SubscribeButton/index.tsx
@@ -37,6 +37,7 @@ export function SubscribeButton () {
       type="button"
       className={styles.subscribeButton}
       onClick={handleSubscribe}
+      disabled
     >
       Subscribe now
     </button>
