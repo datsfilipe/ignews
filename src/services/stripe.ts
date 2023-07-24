bc059d246a601a43fa99a0cddec1dee7d0a7ac64
datsfilipe <datsfilipe.foss@proton.me>
2023-07-24 15:42:51 -0300

    fix: create webhook locally to use custom api version

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -11,3 +11,19 @@ export const stripe = new Stripe(
     }
   }
 )
+
+stripe.webhookEndpoints.list({
+  limit: 1
+}).then(res => {
+  if (!(res.data.length > 0)) {
+    stripe.webhookEndpoints.create({
+      url: 'https://ignews-beige.vercel.app/api/webhooks',
+      api_version: '2020-08-27',
+      enabled_events: [
+        'checkout.session.completed',
+        'customer.subscription.updated',
+        'customer.subscription.deleted'
+      ],
+    }).then(() => console.log('webhook created'))
+  }
+})
